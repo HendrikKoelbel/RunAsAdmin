671bc0f86f2297b354be8aa459507de0fb68221a
Hendrik Kölbel <Hendrik.Koelbel@icloud.com>
2020-06-24 13:00:04 +0200

    Projektdateien hinzufügen.

---
+++ RunAsAdmin/App.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using RunAsAdmin.Views;
+
+namespace RunAsAdmin
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            //initialize the splash screen and set it as the application main window
+            //var splashScreen = new SplashScreenWindow();
+            //this.MainWindow = splashScreen;
+            //splashScreen.Show();
+
+            //in order to ensure the UI stays responsive, we need to
+            //do the work on a different thread
+            Task.Factory.StartNew(() =>
+            {
+                System.Threading.Thread.Sleep(100); // 1000
+                ////we need to do the work in batches so that we can report progress
+                //for (int i = 1; i <= 100; i++)
+                //{
+                //    //simulate a part of work being done
+                //    System.Threading.Thread.Sleep(30);
+
+                //    //because we're not on the UI thread, we need to use the Dispatcher
+                //    //associated with the splash screen to update the progress bar
+                //    splashScreen.Dispatcher.Invoke(() => splashScreen.Progress = i);
+                //}
+
+                //once we're done we need to use the Dispatcher
+                //to create and show the main window
+                this.Dispatcher.Invoke(() =>
+                {
+                    //initialize the main window, set it as the application main window
+                    //and close the splash screen
+                    var mainWindow = new MainWindow();
+                    this.MainWindow = mainWindow;
+                    mainWindow.Show();
+                    //splashScreen.Close();
+                });
+            });
+        }
+
+    }
+}
---
+++ RunAsAdmin/Views/MainWindow.xaml.cs
@@ -0,0 +1,97 @@
+﻿using MahApps.Metro.Controls;
+using MahApps.Metro.Controls.Dialogs;
+using MahApps.Metro.IconPacks;
+using System;
+using System.Net.Http;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace RunAsAdmin.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitializeUpdater();
+        }
+
+
+        #region Update Section
+        readonly CancellationTokenSource UpdateCheckCts = new CancellationTokenSource();
+        public void InitializeUpdater()
+        {
+            //Task task = Task.Run(async () =>
+            //{
+            //    while (true)
+            //    {
+            //        using (var manager = new UpdateManager(new GithubPackageResolver(GlobalUpdater.githubUser, GlobalUpdater.githubProject, GlobalUpdater.githubAssetName), new ZipPackageExtractor()))
+            //        {
+            //            var result = await manager.CheckForUpdatesAsync();
+            //            if (result.CanUpdate)
+            //            {
+            //                UpdateBadge.Invoke(new Action(() =>
+            //                {
+            //                    if (this.UpdateBadge.Badge == null)
+            //                    {
+            //                        this.UpdateBadge.Badge = new PackIconMaterial() { Kind = PackIconMaterialKind.Update };
+            //                    }
+            //                }));
+            //            }
+            //            else
+            //            {
+            //                UpdateBadge.Invoke(new Action(() =>
+            //                {
+            //                    this.UpdateBadge.Badge = null;
+            //                }));
+            //            }
+            //        }
+            //        await Task.Delay(300000, UpdateCheckCts.Token);
+            //    }
+            //}, UpdateCheckCts.Token);
+        }
+        private async void btnUpdate_Click(object sender, RoutedEventArgs e)
+        {
+            //try
+            //{
+            //     Configure to look for packages in specified directory and treat them as zips
+            //    using (var manager = new UpdateManager(new GithubPackageResolver(GlobalUpdater.githubUser, GlobalUpdater.githubProject, GlobalUpdater.githubAssetName), new ZipPackageExtractor()))
+            //    {
+            //         Check for updates
+            //        var result = await manager.CheckForUpdatesAsync();
+            //        if (result.CanUpdate)
+            //        {
+            //            MessageDialogResult dialogResult = await this.ShowMessageAsync("New update available",
+            //             String.Format("A new version is available.\nOld version: {0}\nNew version: {1}\nDo you want to update?",
+            //             Assembly.GetExecutingAssembly().GetName().Version,
+            //             result.LastVersion)
+            //             , MessageDialogStyle.AffirmativeAndNegative);
+            //            if (dialogResult == MessageDialogResult.Affirmative)
+            //            {
+            //                UpdateWindow updateWindow = new UpdateWindow();
+            //                updateWindow.ShowDialog();
+            //            }
+            //        }
+            //        else
+            //        {
+            //            await this.ShowMessageAsync("No update available", "There is currently no update available. Please try again later.", MessageDialogStyle.Affirmative);
+            //        }
+            //    }
+            //}
+            //catch (HttpRequestException httpRequestEx)
+            //{
+            //    await this.ShowMessageAsync(httpRequestEx.GetType().Name, httpRequestEx.Message, MessageDialogStyle.Affirmative);
+            //}
+            //catch (Exception Ex)
+            //{
+            //    await this.ShowMessageAsync(Ex.GetType().Name, Ex.Message, MessageDialogStyle.Affirmative);
+            //}
+        }
+        #endregion
+    }
+}
